cc2897cd7e4845940d0bb2efa2354076bddeccc1
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-08 08:48:41 -0300

    feat: add navbar and footer components and configure app tailwind

--- src/Frontend/src/app/pages/home/home.component.ts
+++ src/Frontend/src/app/pages/home/home.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { firstValueFrom } from 'rxjs';
-import { SummaryCardComponent, Summary } from '../../components/summary-card/summary-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
